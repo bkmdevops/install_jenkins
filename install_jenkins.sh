145cd576d0175f049efab70f795592a562ff02f2
Ahmet Kasım Erbay <ahmetkasim2@gmail.com>
2024-03-20 13:10:26 +0300

    Create install_jenkins.sh

---
+++ install_jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install java-$1-openjdk-devel -y
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install jenkins-$2 -y
+echo "Check the Java version below"
+update-alternatives --config java
+echo "Change default port 8080 to another one in the config file."
+echo "Press any button to go on to the file"
+read -s -n 1
+vi /usr/lib/systemd/system/jenkins.service
+systemctl start jenkins
